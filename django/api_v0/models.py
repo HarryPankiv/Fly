b6fd65bf09cdf6d37f3c7491f9942a375cd44501
Yuriy <zezarthebest@gmail.com>
2017-10-07 16:55:06 +0300

    fix models

--- django/api_v0/models.py
+++ django/api_v0/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Flight(models.Model):
+    destination = models.CharField(max_length=100)
+    price = models.IntegerField(max_length=4)
+    duration = models.CharField(max_length=150)
+
+class Hotel(models.Model):
+    city = models.CharField(max_length=100)
+    stars = models.IntegerField()
