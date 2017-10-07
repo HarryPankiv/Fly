7018b7b1fee5790336e301b5a1ee86970a90604a
Yuriy <zezarthebest@gmail.com>
2017-10-07 17:34:10 +0300

    create model connections_new

--- django/api_v0/models.py
+++ django/api_v0/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 
 
-class Flight(models.Model):
-    destination = models.CharField(max_length=100)
-    price = models.IntegerField(default='0')
-    duration = models.CharField(max_length=150, default='0')
+class City(models.Model):
+    city = models.CharField(max_length=100)
+    flight_price = models.IntegerField(default='0')
+    flight_duration = models.CharField(max_length=150, default='0')
+    activities = models.ManyToManyField(Activity)
 
 
 class Hotel(models.Model):
-    city = models.ForeignKey(Flight, on_delete=models.SET_NULL)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL)
     stars = models.IntegerField(default='0')
     price = models.IntegerField(default='0')
 
 
 class Activity(models.Model):
-    city = models.ManyToManyField(Flight)
+    city = models.ManyToManyField(City)
     price = models.IntegerField(default='0')
     """a1 = Activity()
     a2 = Activity()
     a3 = Activity()
-    f1 = Flight()
+    f1 = City()
     f1.add(a1)
     f1.add(a2)
     f1.add(a3)
