49ec82c51020b9bb053ae24107b7e0742c709f36
Yuriy <zezarthebest@gmail.com>
2017-10-07 17:29:06 +0300

    create model connections

--- django/api_v0/models.py
+++ django/api_v0/models.py
@@ -8,6 +8,26 @@ class Flight(models.Model):
 
 
 class Hotel(models.Model):
-    city = models.CharField(max_length=100)
+    city = models.ForeignKey(Flight, on_delete=models.SET_NULL)
     stars = models.IntegerField(default='0')
     price = models.IntegerField(default='0')
+
+
+class Activity(models.Model):
+    city = models.ManyToManyField(Flight)
+    price = models.IntegerField(default='0')
+    """a1 = Activity()
+    a2 = Activity()
+    a3 = Activity()
+    f1 = Flight()
+    f1.add(a1)
+    f1.add(a2)
+    f1.add(a3)
+    f2 = Flight()
+    f2.add(a1)
+    f2.add(a2)
+    f2.add(a3)
+    f3 = Flight()
+    f3.add(a1)
+    f3.add(a2)
+    f3.add(a3)"""
