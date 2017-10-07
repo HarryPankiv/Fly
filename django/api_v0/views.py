cc92a7559c8afa0c58682fe57bf29080905d6eb7
bohdankaminskyi <b.kaminskyi99@gmail.com>
2017-10-07 16:50:13 +0300

    added

--- django/api_v0/views.py
+++ django/api_v0/views.py
@@ -1,12 +1,28 @@
 from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
 from django.conf import settings
+from .models import Flight
 import json
+import urllib2
+from bs4 import BeautifulSoup
+
+
+
+
 
 import os
+def populate():
+    wiki = 'https://avia.tickets.ua/en/m/search/results?session_id=93dd9629e05d0bedcba1ecc71a892e9a'
+    page = urllib2.urlopen(wiki)
+    soup = BeautifulSoup(page)
+    print soup.prettify()
+
+
+
 
 class Test(View):
     def get(self, request):
+        populate()
         return JsonResponse({'request-type': 'GET'})
 
     def post(self, request):
--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -18,7 +18,7 @@ export default class Header extends Component {
 	}
 
 	startTrip(ev) {
-	    axios.post('http://127.0.0.1:8000/api/v0/', {fuck: 'fuck you'})
+	    axios.get('http://127.0.0.1:8000/api/v0/')
 	    .then(res => {
             console.log(res);
 	    }).then(response => {
