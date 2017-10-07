af681c0355477897c45c92018b014bb8da9a100d
Oleksandr Padko <sasha.pad@gmail.com>
2017-10-07 14:03:45 +0300

    Add header

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import '../css/style.css';
 
 export default class Header extends Component {
 	render() {
 		return (
 			<div>
-				<div>here will be a photo!</div>
+				<div className="ui clearing segment header-segment">
+				  <div className="choosemytrip-button-wrapper">
+				  		<button className="ui center green basic button choosemytrip-button black">Choose my trip</button>
+				  </div>
+				  
+				</div>
 				<button type="button" onClick={this.startTrip}>Start your trip</button>
 			</div>
 		)
