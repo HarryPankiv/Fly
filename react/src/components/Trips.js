20cdddaa4f5806326c1267921b9853efa35b2210
Oleksandr Padko <sasha.pad@gmail.com>
2017-10-07 19:06:53 +0300

    Wraped Trips with a div

--- react/src/components/Trips.js
+++ react/src/components/Trips.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import '../css/style.css';
+import '../css/trips.css';
 import Trip from './Trip.js';
 
 export default class Trips extends Component {
 	render() {
 		let trips = [<Trip/>, <Trip/>, <Trip/>];
 		return (
-			<div>
+			<div className="trip-wrapper">
 				{trips}
 			</div>
 		)
