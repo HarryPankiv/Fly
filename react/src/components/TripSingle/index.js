e90ead82e45cb54a702a472b9eeebd441b26cd46
Oleksandr Padko <sasha.pad@gmail.com>
2017-10-08 01:39:47 +0300

    Update Trip component

--- react/src/components/TripSingle/index.js
+++ react/src/components/TripSingle/index.js
@@ -5,18 +5,29 @@ import Directions from '../Directions/index';
 export default class TripSingle extends Component {
 	render() {
 		let place = 'Vienna';
-		let totalPrice = '$10000';
 		// let tickets;
 		// let hotels;
+		let activity = this.props.activity.charAt(0).toUpperCase() + this.props.activity.slice(1);
+		let city = this.props.city;
+		let activityPrice = parseInt(this.props.activityPrice);
+		let personCount = parseInt(this.props.personCount);
+		let days = parseInt(this.props.days);
+
+		let hotelPrice = parseInt(this.props.hotelPricePP) * days;
+		let flightPrice = parseInt(this.props.flightPricePP) * personCount * 2;
+
+		let totalPrice = hotelPrice * days * personCount + activityPrice + flightPrice * personCount * 2;
 
 		return (
 			<div className="ui segment">
-				<h2 className="ui header trip-header">{place}</h2>
-				<p className="trip-price">{totalPrice}</p>
+				<h2 className="ui header trip-header">{city} -  {activity}</h2>
+				<p className="trip-price"><b>${totalPrice}</b></p>
 
-				<h3 className="ui header">Tickets</h3>
-				<h3 className="ui header">Hotels</h3>
-				<Directions destination={place}/>
+
+				<p className="ui">{activity} price: <b>${flightPrice}</b> </p>
+				<p className="ui">Tickets: <b>${flightPrice}</b> </p>
+				<p className="ui">Hotel price: <b>${hotelPrice}</b> </p>
+				<Directions destination={city}/>
 			</div>
 		)
 	}
--- react/src/components/Trips/index.js
+++ react/src/components/Trips/index.js
@@ -4,7 +4,7 @@ import Trip from '../TripSingle/index';
 
 export default class Trips extends Component {
 	render() {
-		let trips = [<Trip key='1'/>, <Trip key='2'/>, <Trip key='3'/>];
+		let trips = [<Trip key='1' activity="skydiving" city="Vienna" hotelPricePP="550" flightPricePP="450" activityPrice="33" personCount="3" days="4"/>];
 		return (
 			<div className="trip-wrapper">
 				{trips}
