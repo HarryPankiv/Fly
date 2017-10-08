5ca8a8c93b39ca76d8c3fcb76598834c20bb6574
Oleksandr Padko <sasha.pad@gmail.com>
2017-10-08 06:01:41 +0300

    Add random responses

--- django/api_v0/views.py
+++ django/api_v0/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 import json
 import urllib2
 import os
+import random
 
 class Test(View):
     def get(self, request):
@@ -12,6 +13,14 @@ class Test(View):
     def post(self, request):
         print request.body
         json_string = json.loads(request.body)
+        print 'json string', json_string['tags']
+
+        money = json_string['money']
+        tags = json_string['tags']
+
+        if money is not None and tags:
+            return self.getFakeJson(10, money, tags)
+
         return JsonResponse({
             'data': [
                 {
@@ -42,3 +51,47 @@ class Test(View):
                     'flightPrice': 100
                 }
             ]})
+
+    def getFakeJson(self, num, price, tags):
+        if price <= 10:
+            return JsonResponse({
+            'data': []
+        })
+        activities = ['climbing', 'swimming', 'museums', 'skydiving', 'diving', 'sightseeing', 'photography', 'eat out', 'paintball', 'lasertag']
+        cities = ['London', 'Paris', 'Lviv', 'Kiev', 'Warsaw', 'Krakow', 'Berlin', 'Munich', 'Vienna', 'Venice']
+        trips = []
+
+        random.seed(price)
+
+        for i in range(random.randint(3,10)):
+            cityIndx = random.randint(0,len(cities)-1)
+            activityIndx = random.randint(0,len(activities)-1)
+
+            city = cities[cityIndx]
+            activity = activities[activityIndx]
+
+            total = price
+            hotelPrice = random.randint(0, total)
+            #total -= hotelPrice
+            flightPrice = random.randint(0, total)
+            #total -= hotelPrice
+            activityPrice = random.randint(0, total)
+            #total -= activityPrice
+
+            trips.append({
+                    'activity': activity,
+                    'city': city,
+                    'tags': ['photography', 'sightseeing', 'sky-diving'],
+
+                    'activityPrice': activityPrice,
+                    'hotelPrice': hotelPrice,
+                    'flightPrice': flightPrice
+                }
+                )
+
+        return JsonResponse({'data': trips})
+
+
+
+
+
